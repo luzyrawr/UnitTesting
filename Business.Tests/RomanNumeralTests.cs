bccbaba5f9ffd4708237ef6b237ef695d120bdde
luz.galindo <luz.galindo@ipointsystems.com>
2021-09-02 11:14:26 -0500

    RomanNumeral class and tests

---
+++ Business.Tests/RomanNumeralTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Business;
+
+namespace Business.Tests
+{
+    [TestFixture]
+    public class RomanNumeralTests
+    {
+        [TestCase("I",1)]
+        [TestCase("II", 2)]
+        [TestCase("IV", 4)]
+        [TestCase("V", 5)]
+        [TestCase("VI", 6)]
+        [TestCase("IX", 9)]
+        [TestCase("XI", 11)]
+        [TestCase("XIV", 14)]
+        [TestCase("XV", 15)]
+        [TestCase("XVI", 16)]
+        [TestCase("XIX", 19)]
+
+        [TestCase("XX", 20)]
+        [TestCase("XXIV", 24)]
+        [TestCase("XL", 40)]
+        [TestCase("LXXXIX", 89)]
+        [TestCase("XCIV", 94)]
+        [TestCase("XCVIII", 98)]
+        [TestCase("XCIX", 99)]
+
+        [TestCase("CC", 200)]
+        [TestCase("CCXLIV", 244)]
+        [TestCase("CD", 400)]
+        [TestCase("DCCCXCIX", 899)]
+        [TestCase("CMXLIV", 944)]
+        [TestCase("CMLXXXVIII", 988)]
+        [TestCase("CMXCIX", 999)]
+        public void Parse_GivenString_ReturnsInt(string roman, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, RomanNumeral.Parse(roman));            
+        }
+    }
+}
---
+++ Business/RomanNumeral.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business
+{
+    public class RomanNumeral
+    {
+        /*
+        I
+        II
+        III
+        IV
+        V
+        VI
+        VII
+        VIII
+        IX
+
+        X
+        XIV
+        XX
+        XXX
+        XL
+        L
+        LX
+        C
+
+        C
+        CC
+        CCC
+        CD
+        D
+        DC
+        CM
+        M
+         */
+        public static int Parse(string roman)
+        {
+            var result = 0;
+            var romanCharArray = roman.ToCharArray();
+
+            char? previousCharacter = null;
+            foreach(var character in romanCharArray)
+            {
+                if(!previousCharacter.HasValue)
+                {
+                    result += characterValue(character);
+                }
+                else if (characterValue(previousCharacter.Value) >= characterValue(character))
+                {
+                    result += characterValue(character);
+                }
+                else if (characterValue(previousCharacter.Value) < characterValue(character))
+                {
+                    result = result + characterValue(character) - 2*characterValue(previousCharacter.Value);
+                }
+                previousCharacter = character;
+            }
+
+            return result;
+        }
+
+        private static int characterValue(char letter)
+        {
+            switch (letter)
+            {
+                case 'I': return 1;//4,9
+                case 'V': return 5;
+                case 'X': return 10;//40,90
+                case 'L': return 50;
+                case 'C': return 100;//400,900
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
+        }
+    }
+}
